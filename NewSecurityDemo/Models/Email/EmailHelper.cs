2b58112ee0f4307c35c6470b71a4249c8e52cffc
Brendan Wood <Brendanw@mweb.co.za>
2018-10-06 18:22:30 +0100

    Added EAmilHelper Class

---
+++ NewSecurityDemo/Models/Email/EmailHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewSecurityDemo.Models.Email
+{
+    public class EmailHelper
+    {
+        public async void sendMessage(MailDetailModel EmailMessage)
+        {
+            var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+            var message = new MailMessage();
+            message.To.Add(EmailMessage.toAddress);  // replace with valid value 
+            message.From = new MailAddress("sender@outlook.com");  // replace with valid value
+            message.Subject = "Your email subject";
+            message.Body = string.Format(body, EmailMessage.fromName, EmailMessage.fromAddress.Address, EmailMessage.message);
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+
+                    UserName = EmailMessage.CurrentEmailSettings.UserName,  // replace with valid value
+                    Password = EmailMessage.CurrentEmailSettings.Password  // replace with valid value
+                };
+                smtp.Credentials = credential;
+                smtp.Host = EmailMessage.CurrentEmailSettings.SmtpHost;
+                smtp.Port = EmailMessage.CurrentEmailSettings.SmtpPort;
+                smtp.EnableSsl = EmailMessage.CurrentEmailSettings.SslEnabled;
+                await smtp.SendMailAsync(message);
+
+            }
+        }
+    }
+}
