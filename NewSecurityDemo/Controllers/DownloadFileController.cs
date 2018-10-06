11f12025046b235f80c29d920616c50214f403e3
Brendan Wood <Brendanw@mweb.co.za>
2018-10-06 18:30:28 +0100

    Added Download Controler

---
+++ NewSecurityDemo/Controllers/DownloadFileController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewSecurityDemo.Controllers
+{
+    public class DownloadFileController : Controller
+    {
+        private WebDocsEntities db = new WebDocsEntities();
+        // GET: DownloadFile
+
+
+        public ActionResult DownLoadSelectedFile(int FileID)
+        {
+            Models.File ObjFile = GetFileToDownload(FileID);
+            if (ObjFile != null)
+            {
+                return File(ObjFile.FileImage, ObjFile.ContentType, ObjFile.FileName + "." + ObjFile.FileExtension);
+            }
+            else
+            {
+
+                return null;
+            }
+
+
+        }
+
+        private Models.File GetFileToDownload(int _FileID)
+        {
+
+            Models.File fileToDownload;
+
+            fileToDownload = db.Files.Where(a => a.FileID == _FileID).FirstOrDefault<File>();
+            if (fileToDownload != null)
+            {
+                return fileToDownload;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
